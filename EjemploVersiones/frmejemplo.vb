e02e86d42aa34e2fcef798431774264617562e85
icruzfernandez <icruzfernadez@gmail.com>
2017-07-21 09:44:44 -0600

    modificacion de mensaje

--- EjemploVersiones/frmejemplo.vb
+++ EjemploVersiones/frmejemplo.vb
@@ -1,7 +1,7 @@
 ﻿Public Class frmejemplo
     Private Sub btnmensaje_Click(sender As Object, e As EventArgs) Handles btnmensaje.Click
         If Not String.IsNullOrEmpty(txtmensaje.Text) Then
-            MessageBox.Show(txtmensaje.Text)
+            MessageBox.Show("Mensaje ingresado por el usuario: " & txtmensaje.Text)
         Else
             MessageBox.Show("no hay mensaje que mostrar")
         End If
