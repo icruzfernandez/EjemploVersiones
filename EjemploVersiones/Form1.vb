4278cb35cc611516852fc91f7b9d77cc9ba8b47d
icruzfernandez <icruzfernadez@gmail.com>
2017-07-20 19:09:01 -0600

    agregar formulario

--- EjemploVersiones/Form1.vb
+++ EjemploVersiones/Form1.vb
@@ -1,5 +1,7 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MessageBox.Show("hola")
+        Dim frm As New frmejemplo
+        frm.Show()
     End Sub
 End Class
---
+++ EjemploVersiones/frmejemplo.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmejemplo
+
+End Class
