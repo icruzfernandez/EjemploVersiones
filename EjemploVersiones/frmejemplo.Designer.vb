26f1cfc982b759477af6814c6b62041f8c32aa57
icruzfernandez <icruzfernadez@gmail.com>
2017-07-21 09:33:23 -0600

    etiqueta

--- EjemploVersiones/frmejemplo.Designer.vb
+++ EjemploVersiones/frmejemplo.Designer.vb
@@ -22,8 +22,31 @@ Partial Class frmejemplo
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(107, 38)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 24)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Hola"
+        '
+        'frmejemplo
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmejemplo"
         Me.Text = "frmejemplo"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents Label1 As Label
 End Class
